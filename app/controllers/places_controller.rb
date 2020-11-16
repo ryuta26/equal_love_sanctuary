abd4d2e643b50d79292799e851d403aba91d78fc
EC2 Default User <ec2-user@ip-172-31-31-218.ec2.internal>
2020-11-16 06:12:44 +0000

    add song and member to seed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,4 +5,23 @@ class PlacesController < ApplicationController
     
     def index
     end
+    
+    def new
+        @place = Place.new
+    end
+    
+    def create
+        @place = Place.new(place_params)
+        @place.user = current_user
+        if @place.save
+            redirect_to places_path
+        else
+            render new_place_path
+        end
+    end
+    
+    private
+    def place_params
+        params.require(:place).permit(:name,:area,:postal_code,:address,:latitude,:longitude,:user_id,:song_id,:image)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,114 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.create!(
+  [
+    {
+      name: '=LOVE'
+    },
+    {
+      name: '記憶のどこかで'
+    },
+    {
+      name: 'スタート！'
+    },
+    {
+      name: "僕らの制服クリスマス"
+    },
+    {
+      name: '届いてLOVE YOU'
+    },
+    {
+      name: 'ようこそ！イコラブ沼'
+    },
+    {
+      name: '手遅れcaution'
+    },
+    {
+      name: '「部活中に目が合うなって思ってたんだ」'
+    },
+    {
+      name: '樹愛羅、助に来たぞ'
+    },
+    {
+      name: 'Want you!Want you!'
+    },
+    {
+      name: '今、この船に乗れ！'
+    },
+    {
+      name: 'アイカツハッピーエンド'
+    },
+    {
+      name: '探せ ダイヤモンドリリー'
+    },
+    {
+      name: 'いらない ツインテール'
+    },
+    {
+      name: '虹の素'
+    },
+    {
+      name: 'ズルいよ ズルいね'
+    },
+    {
+      name: 'Sweetest girl'
+    },
+    {
+      name: '推しのいる世界'
+    },
+    {
+      name: 'CAMEO'
+    },
+    {
+      name: '「君と私の歌」'
+    },
+    {
+      name: 'My Voice Is For You'
+    },
+    {
+      name: '青春”サブリミナル”'
+    }
+  ]
+)
+
+Member.create!(
+  [
+    {
+      name: '大谷 映美里'
+    },
+    {
+      name: '大場 花菜'
+    },
+    {
+      name: '音嶋 莉沙'
+    },
+    {
+      name: "齋藤 樹愛羅"
+    },
+    {
+      name: '齊藤 なぎさ'
+    },
+    {
+      name: '佐々木 舞香'
+    },
+    {
+      name: '佐竹 のん乃'
+    },
+    {
+      name: '髙松 瞳'
+    },
+    {
+      name: '瀧脇 笙古'
+    },
+    {
+      name: '野口 衣織'
+    },
+    {
+      name: '諸橋 沙夏'
+    },
+    {
+      name: '山本 杏奈'
+    }
+  ]
+)
